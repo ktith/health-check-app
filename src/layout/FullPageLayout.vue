df26fd9777dde39e2df30d5a4a9e56ac899264ae
k_tith <khemtith@station29.com>
2025-10-21 00:02:52 +0700

    add homepage

--- src/layout/FullPageLayout.vue
+++ src/layout/FullPageLayout.vue
@@ -237,9 +237,7 @@ export default {
 </script>
 
 <style scoped>
-  @import "~@/assets/vendor/bootstrap/css/bootstrap.min.css";
   @import "~@/assets/vendor/bootstrap-icons/bootstrap-icons.css";
-  @import "~@/assets/vendor/aos/aos.css";
   @import "~@/assets/vendor/fontawesome-free/css/all.min.css";
   @import "~@/assets/vendor/glightbox/css/glightbox.min.css";
   @import "~@/assets/vendor/swiper/swiper-bundle.min.css";
